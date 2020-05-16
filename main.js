698628738154ec5bf2d099632b8a9cfd7fb25454
Usama Irfan <usama.irfan9797@gmail.com>
2020-05-17 03:29:37 +0500

    Added Mobile Responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('header');
 const navItemBorders = document.getElementsByClassName('.border__bottom')
-
+const hamburgerContainer = document.querySelector('.hamburger__container')
+const hamburger = document.querySelector('.hamburger');
+const navMobile = document.querySelector('.nav__mobile')
 window.addEventListener('scroll', (e) => {
     if (window.scrollY > 0) {
         header.classList.add("shrinked")
@@ -8,3 +10,8 @@ window.addEventListener('scroll', (e) => {
         header.classList.remove("shrinked")
     }
 });
+
+hamburgerContainer.addEventListener('click', (e) => {
+    hamburgerContainer.classList.toggle('open')
+    navMobile.classList.toggle('open')
+})
