a2bda6e4c5844aa5a2d52e07da567be1ae690a9a
Usama Irfan <usama.irfan9797@gmail.com>
2020-05-30 20:34:41 +0500

    Web Uploaded

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 const header = document.querySelector('header');
-const navItemBorders = document.getElementsByClassName('.border__bottom')
-const hamburgerContainer = document.querySelector('.hamburger__container')
+const navItemBorders = document.getElementsByClassName('.border__bottom');
+const hamburgerContainer = document.querySelector('.hamburger__container');
 const hamburger = document.querySelector('.hamburger');
-const navMobile = document.querySelector('.nav__mobile')
-window.addEventListener('scroll', (e) => {
+const navMobile = document.querySelector('.nav__mobile');
+const sub_items_mobile = document.querySelector('.sub-item');
+const dropdownsMobile = document.querySelector('.dropdowns')
+window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-        header.classList.add("shrinked")
+        header.classList.add("shrinked");
     } else {
-        header.classList.remove("shrinked")
+        header.classList.remove("shrinked");
     }
 });
 
-hamburgerContainer.addEventListener('click', (e) => {
-    hamburgerContainer.classList.toggle('open')
-    navMobile.classList.toggle('open')
+hamburgerContainer.addEventListener('click', () => {
+    hamburgerContainer.classList.toggle('open');
+    navMobile.classList.toggle('open');
+});
+
+dropdownsMobile.addEventListener('click', () => {
+    sub_items_mobile.classList.toggle('open')
 })
